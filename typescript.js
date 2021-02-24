8bfe534ab4e514ac1728f85c85b5b2caa54f489d
mark sauer-utley <mark.sauerutley@respondent.io>
2021-02-24 18:14:35 -0400

    update required modules

--- typescript.js
+++ typescript.js
@@ -1,6 +1,11 @@
 module.exports = {
 	extends: [
 		'airbnb-typescript/base',
-		'./index.js',
+		require.resolve('./index.js'),
 	],
+	rules: {
+		'@typescript-eslint/indent': [2, 'tab'],
+		'no-shadow': 'off',
+		'@typescript-eslint/no-shadow': ['error'],
+	},
 };
